c675fea5434f353041793a37dee803ed082df317
@matibdr7 <matias.bdr7@gmail.com>
2024-10-29 00:39:27 -0300

    Base para Punto2

---
+++ src/Punto2/App.java
@@ -0,0 +1,16 @@
+/*
+    Considerando la clase BinaryTree<T> comentada en teoría, construir los árboles binarios correspondientes a
+    las figuras A y B presentadas más abajo. Imprimir en pantalla la representación parentizada de los árboles.
+    Además, indicar las salidas correspondientes a los recorridos in order, pre order, post order y descending
+    order de los árboles:
+
+    [Figura en docs/TP6-ED2024-2C.pdf (Punto2)]
+*/
+
+package Punto2;
+
+public class App {
+    public static void main(String[] args){
+
+    }
+}
