6c59c4e8f607289d22bada175be8385056d84d24
@matibdr7 <matias.bdr7@gmail.com>
2024-10-29 00:05:55 -0300

    Estructura de Arbol generada, raiz: nodo459

--- src/Punto1/App.java
+++ src/Punto1/App.java
@@ -12,13 +12,18 @@ d. Determinar el máximo nivel del árbol.
 e. Determinar la altura del árbol.
 f. Recorrer el árbol en orden descendente.
 g. Imprimir en pantalla la representación parentizada del árbol.
-1
-
  */
 package Punto1;
 
 public class App {
     public static void main(String[] args){
+        BinaryTree<Integer> nodo939 = new BinaryTree<>(939,new BinaryTree<Integer>(925),null);
+        BinaryTree<Integer> nodo893 = new BinaryTree<>(893,null,nodo939);
+        BinaryTree<Integer> nodo744 = new BinaryTree<>(744, new BinaryTree<Integer>(565), null);
+        BinaryTree<Integer> nodo473 = new BinaryTree<>(473, null, nodo744);
+        BinaryTree<Integer> nodo812 = new BinaryTree<>(812, nodo473, nodo893);
+        BinaryTree<Integer> nodo10 = new BinaryTree<>(10, null, new BinaryTree<Integer>(186));
+        BinaryTree<Integer> nodo459 = new BinaryTree<>(459, nodo10, nodo812);
 
-    }
+    }   
 }
