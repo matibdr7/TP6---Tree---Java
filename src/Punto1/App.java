00adbd0fcd1c72309f5301644201dbed2f08943e
@matibdr7 <matias.bdr7@gmail.com>
2024-10-28 23:48:33 -0300

    Base para el Punto1

---
+++ src/Punto1/App.java
@@ -0,0 +1,24 @@
+/*
+ Considerando la clase BinaryTree<T> comentada en teoría, construir un árbol binario correspondiente a la
+siguiente figura.
+
+[Figura en docs/TP6-ED2024-2C.pdf (Punto1)]
+
+A partir del árbol obtenido:
+a. Calcular la cantidad de nodos del árbol.
+b. Calcular la cantidad de hojas del árbol.
+c. Calcular la cantidad de nodos internos del árbol.
+d. Determinar el máximo nivel del árbol.
+e. Determinar la altura del árbol.
+f. Recorrer el árbol en orden descendente.
+g. Imprimir en pantalla la representación parentizada del árbol.
+1
+
+ */
+package Punto1;
+
+public class App {
+    public static void main(String[] args){
+
+    }
+}
